8f030d64936f6645cef09272fec0b39b82dc54e3
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-07-05 08:57:21 -0300

    Metodo Input

---
+++ Input.cs
@@ -0,0 +1,10 @@
+using System;
+    class Hola{
+        static void Main(string[]args){
+            Console.WriteLine("Escriba su nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Escriba su edad");
+            String edad = Console.ReadLine();
+            Console.WriteLine("Hola "+nombre+" tienes "+edad+" años de edad");
+        }
+    }
