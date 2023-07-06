9ab47c983f679c23d7209df8e9cdbbf421253014
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-07-06 00:40:19 -0300

    Create TiposDatos.cs

---
+++ TiposDatos.cs
@@ -0,0 +1,32 @@
+using System;
+    class Hola{
+        static void Main(string[]args){
+            byte byteVar = 10;
+            sbyte sbyteVar = 11;
+            int intVar = 12;
+            uint unitVar = 13;
+            short shortVar = 14;
+            ushort ushortVar = 15;
+            long longVar = 16;
+            ulong ulongVar = 17;
+            float floatVar = 18.18f;
+            double doubleVar = 19.19;
+            decimal decimalVar = 20.20m;
+            char charVar = 'S';
+            string stringVar = "Hola";
+
+            Console.WriteLine("byte = {0}",byteVar);
+            Console.WriteLine("sbyte = {0}",sbyteVar);
+            Console.WriteLine("int = {0}",intVar);
+            Console.WriteLine("unit = {0}",unitVar);
+            Console.WriteLine("ushort = {0}",ushortVar);
+            Console.WriteLine("short = {0}",shortVar);
+            Console.WriteLine("long = {0}",longVar);
+            Console.WriteLine("ulong = {0}",ulongVar);
+            Console.WriteLine("float = {0}",floatVar);
+            Console.WriteLine("double = {0}",doubleVar);
+            Console.WriteLine("decimal = {0}",decimalVar);
+            Console.WriteLine("char = {0}",charVar);
+            Console.WriteLine("string = {0}",stringVar);
+        }
+    }
