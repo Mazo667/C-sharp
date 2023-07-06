ca97f7f8041c3f77b60eb21f38fb77072d530f2b
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-07-06 02:17:51 -0300

    Create Comillas.cs

---
+++ Comillas.cs
@@ -0,0 +1,7 @@
+using System;
+    class Hola{
+        static void Main(string[]args){
+            string quote = "Life is an \"Open Secret\". \nEverything is you cant hidden";
+            Console.WriteLine(quote);
+        }
+    }
