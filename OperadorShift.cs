72effc7e7c52a1d395e07f9efca408fde819493d
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-07-06 19:59:41 -0300

    Create OperadorShift.cs

---
+++ OperadorShift.cs
@@ -0,0 +1,14 @@
+using System;
+class OperadorLogicoAND {
+	public static void Main() {
+	 byte byteVar = 7 ;
+        Console.WriteLine("Value of Byte variable is " + byteVar);
+        Console.WriteLine("After shifting 1 bit left " + (byteVar << 1 ));
+        Console.WriteLine("After shifting 2 bits left " + (byteVar << 2 ));
+        Console.WriteLine("After shifting 3 bits left " + (byteVar << 3 ));
+        Console.WriteLine("After shifting 4 bits left " + (byteVar << 4 ));
+        Console.WriteLine("After shifting 5 bits left " + (byteVar << 5 ));
+        Console.WriteLine("After shifting 6 bits left " + (byteVar << 6 ));
+        Console.WriteLine("Value of Byte variable is " + byteVar);
+    }
+}
