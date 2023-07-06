7e75b5cc72fba18f9d389788b155554ad95d60a6
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-07-06 19:45:36 -0300

    Create OperadorLogico!.cs

---
+++ OperadorLogico!.cs
@@ -0,0 +1,9 @@
+using System;
+class OperadorLogicoAND {
+	public static void Main() {
+		bool a = true ;
+        Console.WriteLine("a = {0}, !a = {1}", a, !a);
+        a = false ;
+        Console.WriteLine("a = {0}, !a = {1}", a, !a);
+    }
+}
